7daf39a1cbc7579f7f35fc8e166e70ce9b4068b1
Thiago Chrisostomo <thiago.crisostomo@hotmail.com>
2024-01-10 19:32:48 -0300

    alterando mensagem de acerto

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function verificarChute() {
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Acertou');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Parabéns!Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `Parabéns!!Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
