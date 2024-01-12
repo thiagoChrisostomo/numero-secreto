db1a0ebfca82b26be7fb84b2908ad844a38f48b9
Thiago Chrisostomo <thiago.crisostomo@hotmail.com>
2024-01-12 18:23:44 -0300

    ALTERAÇÃO NO NOME DO JOGO

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1', 'JOGO DO NÚMERO SECRETO');
+    exibirTextoNaTela('h1', 'OGO DO NÚMERO SECRETO');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
